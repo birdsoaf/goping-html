cc2604eac850ff0590ffdef076b03ca042513e2a
Alex Shulman <alex@maraxi.com>
2014-12-22 10:08:29 -0800

    Moved files around

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/googollee/go-socket.io"
 	"github.com/tatsushid/go-fastping"
@@ -11,11 +12,17 @@ import (
 	"time"
 )
 
-type response struct {
-	url   string
-	addr  string
-	rtt   time.Duration
-	epoch int64
+type Response struct {
+	Url   string
+	Addr  string
+	Rtt   time.Duration
+	Epoch int64
+}
+
+type PingManger struct {
+	p       *fastping.Pinger
+	results map[string]*Response
+	ipTourl map[string]string
 }
 
 func main() {
@@ -23,25 +30,26 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	var pm *PingManger
+	pinglist := make([]string, 2)
+	pinglist[0] = "google.com"
+	pinglist[1] = "cogentco.com"
+	pm = StartPingEmitter(pinglist, server)
+	// p = StartPingEmitter("google.com", server)
+
 	server.On("connection", func(so socketio.Socket) {
 		// Store this by connection ID or something
-		var p *fastping.Pinger
 		log.Println("on connection")
 		so.Join("chat")
 		so.On("chat message", func(msg string) {
 			fmt.Println(msg)
 			switch {
 			case msg == "start ping":
-				p = StartPingEmitter("google.com", so)
+				fmt.Print("Start Ping (Already Started)")
 			case msg == "stop ping":
-				p.Stop()
+				fmt.Print("Stop Ping")
+				pm.p.Stop()
 			}
-
-			// Old single call pinger.
-			// onRecv := make(chan response)
-			// fastpinger("google.com", onRecv)
-			// res := <-onRecv
-			// fmt.Println(res.rtt)
 			log.Println("emit:", so.Emit("chat message", msg))
 			so.BroadcastTo("chat", "chat message", msg)
 		})
@@ -55,92 +63,74 @@ func main() {
 
 	http.Handle("/socket.io/", server)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/add/{url}", handler)
 	log.Println("Serving at localhost:5000...")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
 
-func StartPingEmitter(url string, so socketio.Socket) *fastping.Pinger {
-	p := fastping.NewPinger()
+func AddURL(rw http.ResponseWriter, req *http.Request, pm PingManger) {
+
+}
+
+func StartPingEmitter(urls []string, server *socketio.Server) *PingManger {
+	pm := PingManger{
+		p:       fastping.NewPinger(),
+		results: make(map[string]*Response),
+		ipTourl: make(map[string]string),
+	}
 	netProto := "ip4:icmp"
-	if strings.Index(url, ":") != -1 {
+	if strings.Index(urls[0], ":") != -1 {
 		netProto = "ip6:ipv6-icmp"
 	}
-	ra, err := net.ResolveIPAddr(netProto, url)
-	if err != nil {
-		return p
+	for _, element := range urls {
+		ra, err := net.ResolveIPAddr(netProto, element)
+		if err != nil {
+			return &pm
+		}
+		pm.ipTourl[ra.String()] = element
+		pm.results[ra.String()] = nil
+		pm.p.AddIPAddr(ra)
 	}
-	results := make(map[string]*response)
-	results[ra.String()] = nil
-	p.AddIPAddr(ra)
-	onRecv, onIdle := make(chan *response), make(chan bool)
-	p.OnRecv = func(addr *net.IPAddr, t time.Duration) {
-		onRecv <- &response{url: url, addr: addr.String(), rtt: t, epoch: time.Now().Unix()}
+	onRecv, onIdle := make(chan *Response), make(chan bool)
+	pm.p.OnRecv = func(addr *net.IPAddr, t time.Duration) {
+		onRecv <- &Response{Url: pm.ipTourl[addr.String()], Addr: addr.String(), Rtt: t, Epoch: time.Now().Unix()}
 	}
-	p.OnIdle = func() {
+	pm.p.OnIdle = func() {
 		onIdle <- true
 	}
 
-	p.MaxRTT = time.Second
-	p.RunLoop()
+	//p.MaxRTT = time.Second
+	pm.p.MaxRTT = time.Millisecond * 250
+	pm.p.RunLoop()
 	go func() {
 		for {
 			select {
 			case res := <-onRecv:
-				if _, ok := results[res.addr]; ok {
-					results[res.addr] = res
+				if _, ok := pm.results[res.Addr]; ok {
+					pm.results[res.Addr] = res
 				}
 			case <-onIdle:
-				for host, r := range results {
+				for host, r := range pm.results {
 					if r == nil {
 						fmt.Printf("%s : unreachable %v\n", host, time.Now())
+						server.BroadcastTo("chat", "ping message", "0")
 					} else {
-						msg := fmt.Sprintf("%s : %v %v\n", host, r.rtt, time.Now())
-						fmt.Print(msg)
-						so.Emit("ping message", strings.Replace(r.rtt.String(), "ms", "", -1))
+						// msg := fmt.Sprintf("%s : %v %v\n", host, r.rtt, time.Now())
+						// fmt.Print(msg)
+						b, _ := json.Marshal(pm.results[host])
+						// fmt.Println(results[host])
+						// fmt.Println(string(b))
+						server.BroadcastTo("chat", "ping message", string(b))
 					}
-					results[host] = nil
+					pm.results[host] = nil
 				}
 			// case <-done:
 			// 	fmt.Println("HERE")
 			// 	p.Stop()
-			case <-p.Done():
-				if err = p.Err(); err != nil {
-					fmt.Println("Ping failed:", err)
-				}
+			case <-pm.p.Done():
 				break
 			}
 		}
 	}()
-	return p
-}
-
-func fastpinger(url string, onRecv chan<- response) {
-	p := fastping.NewPinger()
-	netProto := "ip4:icmp"
-	if strings.Index(url, ":") != -1 {
-		netProto = "ip6:ipv6-icmp"
-	}
-	ra, err := net.ResolveIPAddr(netProto, url)
-	if err != nil {
-		return
-	}
-	p.AddIPAddr(ra)
-	//onRecv := make(chan *response)
-	onIdle := make(chan bool)
-	p.OnRecv = func(addr *net.IPAddr, t time.Duration) {
-		epoch := time.Now().Unix()
-		fmt.Println("here")
-		onRecv <- response{url: url, addr: addr.String(), rtt: t, epoch: epoch}
-		fmt.Println(t)
-		p.Stop()
-	}
-	p.OnIdle = func() {
-		onIdle <- true
-	}
-	p.MaxRTT = time.Second
-	p.RunLoop()
-	fmt.Println("here")
-	return
-	// fmt.Println(res.rtt)
-	// fmt.Printf("ping.%s %s %v\n", url,  strings.Replace(r.rtt.String(), "ms","", -1), time.Now().Unix())
+	return &pm
 }
